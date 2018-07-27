c044b0b862fe663cfe0c88e2b7c12e5ac329d85e
yeondeok kim <uyan21@naver.com>
2018-07-27 14:46:15 +0900

    Initial commit

---
+++ serverled/serverled.ino
@@ -0,0 +1,71 @@
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#define LED D1
+#define PORT 0/*수정*/
+const char* ssid  = "SSID";/*수정*/
+const char* password = "PASSWORD";/*수정*/
+const char* server = "SERVER_URL";/*수정*/
+int val;/*킬지 끌지 받는 변수 */
+String jsondata="";/*전송용 데이터지만 무슨 이유인지 아두이노가 받기만 해도 형식상 보내 줘야 됩니다. */
+String rcvbuf;/*서버로 부터 받은 json을 받는 변수*/
+char c;/*한 글자 한 글자 읽어서 rcvbuf에 쌓는 변수*/
+int parsing_run=0;/*{(중괄호) 부터 }(중괄호)까지 데이터에 쌓게 만들기위해서 토글 형식으로 사용하려고 할당
+(제가 처음 ArduinoJson.h 다룰때 만든 사람이 파싱하는거 완성을 못 해놔서 제가 이런 식으로 만들어서 쓰고있습니다.)*/
+WiFiClient client;
+StaticJsonBuffer<200> jsonBuffer;/*Json 클래스 할당*/
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    }
+  Serial.println("");
+  Serial.println("MCU has been connected.");
+  Serial.println("Router name : "+String(ssid));  
+  Serial.print("Allocated IP address from Router is ");
+  Serial.println(WiFi.localIP());
+}
+void loop() {
+act(1000);/*매개변수가 딜레이*/
+}
+
+void act(int del){
+  delay(del);
+  client.stop();
+if(client.connect(server,PORT)){
+  /*형식
+    POST / HTTP 1.1
+    Host: URL
+    Connextion: close
+    Content-Type: json
+    Content-Length: sizeof(jsondata)
+    jsondata <-이거 공갈로 라도 보내야 json받아 옵니다.
+    */
+  String s="Host: "+String(server);
+  client.println("POST /arduino HTTP/1.1");
+  client.println(s);
+  client.println("Connection: close");
+  client.println("Content-Type: json");
+  client.print("Content-Length: ");
+  client.println(jsondata.length());
+  client.println();
+  client.println(jsondata);
+  
+  while(client.available()){
+    c=client.read();
+    if(c=='{'){parsing_run=1;}/*토글 형식으로 켜지면 rcvbuf에 쌓게 만듦*/
+    if(parsing_run==1){rcvbuf+=c;}
+    if(c=='}'){parsing_run=0;}  /*꺼지면 끝*/
+      }
+      JsonObject&root=jsonBuffer.parseObject(rcvbuf);/*코딩하기 쉽게 json파일로 변환*/
+      Serial.println(rcvbuf);
+      val=root["option"];/*option: 키에서 값을 받고 val에 할당 */
+      if(val==1){digitalWrite(LED,HIGH);}
+      else if(val==0){digitalWrite(LED,LOW);}
+      rcvbuf="";/*받은 값 초기화*/
+    }
+    else{Serial.println("NO");}
+    }
+
---
+++ test01.py
@@ -0,0 +1,114 @@
+import os,datetime
+from flask import Flask,render_template,redirect,request,jsonify
+app=Flask(__name__)
+option=time=lhum=ltem=0
+
+ntime=datetime.datetime.now()
+@app.route('/')
+def door():
+    #동적 html코드
+    return render_template('pr.html')
+@app.route('/c',methods=['POST'])
+def command():
+    global option,time
+    #html에서 button값을 받음
+    option=request.form['button']
+    #예약 모드면 예약 모드 제거
+    time=0
+    #'/'로 가서 pr.html다시 렌더링
+    return redirect('/')
+@app.route('/t',methods=['POST'])
+def timecommand():
+    #stime 시작시간 etime 종료시간
+    global stime,etime,time
+    #예약 모드시 시간값을 받음
+    time=request.form['time']
+    shour=int(request.form['shour'])
+    smin=int(request.form['smin'])
+    ssec=int(request.form['ssec'])
+    ehour=int(request.form['ehour'])
+    emin=int(request.form['emin'])
+    esec=int(request.form['esec'])
+    #연산이 가능하게 시간 형식 표준화 시작시간, 종료시간을
+    #당일 0시 0분 0초로 초기화
+    stime=datetime.date.strftime(ntime,'%Y-%m-%d')
+    stime=datetime.datetime.strptime(stime, '%Y-%m-%d')
+    etime=datetime.date.strftime(ntime,'%Y-%m-%d')
+    etime=datetime.datetime.strptime(etime, '%Y-%m-%d')
+    #입력받은 시간으로 변환
+    stime+=datetime.timedelta(hours=shour,minutes=smin,seconds=ssec)
+    etime+=datetime.timedelta(hours=ehour,minutes=emin,seconds=esec)
+    #시작시간이 더 앞서 있으면 종료시간을 다음 날로 변환(버그 방지)
+    #ex)시작시간 10시 종료시간 9시 ->종료시간 다음날 9시까지 출력
+    if stime>etime:
+        etime+=datetime.timedelta(days=1)
+    print(stime,etime,time)
+    #'/'로 가서 pr.html다시 렌더링
+    return redirect('/')
+
+@app.route('/cc',methods=['POST'])
+def con_command():
+    global condition,lhum,ltem,time
+    condition=request.form['condition']
+    if condition == 'tem':
+        ltem=request.form['tem']
+        
+    elif condition == 'hum':
+        lhum=request.form['hum']
+    time=0
+    ltem=int(ltem)
+    lhum=int(lhum)
+    print(ltem,lhum,condition,time)
+    return redirect('/')
+
+#MCU가 들어오는 곳
+@app.route('/MCU',methods=['POST'])
+def arduino():
+    global ntime,option
+    #현재시간 갱신
+    ntime=datetime.datetime.now()
+    #예약 모드면 들어감
+    if time:
+        #ex)시작시간 8시이고 현재시간 9시면 켜짐
+        if stime<ntime:
+            option=1
+        #아니면 꺼짐(아직 시작시간이 안됨)
+        else:
+            option=0
+        #종료시간 오버하면 꺼짐
+        if ntime>etime:
+            option=0
+########################시간 조건 경계##########################
+    elif condition:
+        if condition == 'tem':
+            if lhum<=tem:
+                option=1
+            else:
+                option=0
+        elif condition == 'hum':
+            if ltem<=hum:
+                option=1
+            else:
+                option=0
+    return redirect('/')
+
+    #옵션 값을 보내서 제어
+    arduino={"option":option}
+    return jsonify(arduino)
+
+@app.route('/htparse',methods=['POST'])
+def parsercommand():
+    global hum,tem,ntime
+    status=request.get_json()
+    hum=status['hum']
+    tem=status['tem']
+    ntime=datetime.date.now()
+    logtime=datetime.date.strftime(ntime,'%Y-%m-%d %H:%M:%S')
+    f=open('log.txt','a')
+    f.write('%s %d %d'%(logtime,hum,tem))
+    return 'Done'
+
+port = os.getenv('PORT', '2443')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port))
+
